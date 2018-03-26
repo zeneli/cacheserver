43da3134d83c5fe5679386f03203c808a24c5d97
Amarildo Zeneli <tud27246@temple.edu>
2018-03-26 15:27:33 -0400

    cacheserver: Make GetRange concurrency-safe.
    Add fake sequential and concurrent tests that mirror the real range
    cache, but by using simpler []int.
    `go test -v -race` shows GetRange no longer contains race conditions.

--- cacheserver.go
+++ cacheserver.go
@@ -15,15 +15,11 @@ import (
 type CacheServer struct {
 	mu    sync.Mutex // guards cache
 	cache *rangecache.RangeCache
-	url   string
 }
 
 // NewCache returns an initialized CacheServer.
-func NewCacheServer(url string, nbytes int64) *CacheServer {
-	return &CacheServer{
-		cache: rangecache.NewRangeCache(nbytes),
-		url:   url,
-	}
+func NewCacheServer(nbytes int64) *CacheServer {
+	return &CacheServer{cache: rangecache.NewRangeCache(nbytes)}
 }
 
 // ServeHTTP implements the HTTP user interface.
@@ -33,27 +29,47 @@ func NewCacheServer(url string, nbytes int64) *CacheServer {
 // And serve the requested range in a concurrently safe manner.
 func (cs *CacheServer) ServeHTTP(w http.ResponseWriter, r *http.Request) { return }
 
+func (cs *CacheServer) add(keyrange rangecache.Keyrange, body interface{}) {
+	cs.mu.Lock()
+	defer cs.mu.Unlock()
+	switch body.(type) {
+	case []int:
+		cs.cache.Add(keyrange, body.([]int))
+	case []byte:
+		cs.cache.Add(keyrange, body.([]byte))
+	}
+}
+
+func (cs *CacheServer) get(keyrange rangecache.Keyrange) (interface{}, bool) {
+	cs.mu.Lock()
+	defer cs.mu.Unlock()
+	return cs.cache.Get(keyrange)
+}
+
 // GetRange looks in the cache for keyrange start to end.
 // On a miss, GetRange makes an HTTP GET request for the range and
 // stores it in the cache.
 // error is propogated to the calling function.
-func (cs *CacheServer) GetRange(keyrange rangecache.Keyrange) ([]byte, error) {
+// GetRange is concurrency-safe.
+func (cs *CacheServer) GetRange(url string, keyrange rangecache.Keyrange) ([]byte, error) {
 	timeStart := time.Now()
 	rangeHeader := fmt.Sprintf("bytes=%d-%d", keyrange.Start, keyrange.End)
 
-	if v, ok := cs.cache.Get(keyrange); ok { // cache hit
+	v, ok := cs.get(keyrange)
+	if ok { // cache hit
 		body := v.([]byte)
-		// ioutil.WriteFile(rangeHeader+".mp4", v.([]byte), 0x777) // write to file
+		ioutil.WriteFile(rangeHeader+".mp4", v.([]byte), 0x777) // write to file
 		log.Printf("cache hit: %s, GET: %s\n", time.Since(timeStart), rangeHeader)
 		return body, nil
 	}
+
 	// cache miss; make request
-	body, err := httpGetRangeRequest(cs.url, rangeHeader)
+	body, err := httpGetRangeRequest(url, rangeHeader)
 	if err != nil {
 		return nil, err
 	}
-	cs.cache.Add(keyrange, body)
-	//ioutil.WriteFile(rangeHeader+".mp4", []byte(string(body)), 0666)
+	cs.add(keyrange, body)
+	ioutil.WriteFile(rangeHeader+".mp4", []byte(string(body)), 0666)
 	log.Printf("cache miss: %s, GET: %s\n", time.Since(timeStart), rangeHeader)
 	return body, nil
 }
@@ -81,8 +97,9 @@ func main() {
 	// Stub the source URL. Exercise cache hit and miss expectations.
 	// We log the times and save the video files.
 	url := "http://storage.googleapis.com/vimeo-test/work-at-vimeo.mp4"
-	cs := NewCacheServer(url, 64000000)          // 64 MB cache server
-	cs.GetRange(rangecache.Keyrange{0, 6400000}) // 6.4 MB
-	cs.GetRange(rangecache.Keyrange{0, 1600000}) // exact match; 1.6 MB
-	cs.GetRange(rangecache.Keyrange{0, 3200000}) // overlapping match; 3.2 MB
+	cs := NewCacheServer(64000000) // 64 MB cache server
+
+	cs.GetRange(url, rangecache.Keyrange{0, 6400000}) // 6.4 MB
+	cs.GetRange(url, rangecache.Keyrange{0, 1600000}) // exact match; 1.6 MB
+	cs.GetRange(url, rangecache.Keyrange{0, 3200000}) // overlapping match; 3.2 MB
 }
--- cacheserver_test.go
+++ cacheserver_test.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"log"
+	"sync"
 	"testing"
 	"time"
 
 	"github.com/zeneli/cacheserver/rangecache"
 )
 
-var url string = "http://storage.googleapis.com/vimeo-test/work-at-vimeo.mp4"
+const url = "http://storage.googleapis.com/vimeo-test/work-at-vimeo.mp4"
+
 var nbytesMax int64 = 64000000 // 64 MB
 
 // incomingRangeRequests simulates a channel of range requests.
@@ -32,10 +34,31 @@ func incomingRangeRequests() <-chan rangecache.Keyrange {
 	return ch
 }
 
-func testSequential(t *testing.T, cs *CacheServer) {
+// incomingRangeRequestsFake simulates small sporadic range requests.
+// Redundant and large table helps warm up the cache.
+func incomingRangeRequestsFake() <-chan rangecache.Keyrange {
+	ch := make(chan rangecache.Keyrange)
+	go func() {
+		for _, keyrange := range []rangecache.Keyrange{
+			{0, 640}, {0, 320}, {0, 1280}, {0, 1080}, {0, 640},
+			{0, 640}, {0, 320}, {0, 1280}, {0, 1080}, {0, 640},
+			{0, 640}, {0, 320}, {0, 1280}, {0, 1080}, {0, 640},
+			{0, 640}, {0, 320}, {0, 1280}, {0, 1080}, {0, 640},
+			{0, 640}, {0, 320}, {0, 1280}, {0, 1080}, {0, 640},
+			{0, 640}, {0, 320}, {0, 1280}, {0, 1080}, {0, 640},
+		} {
+			ch <- keyrange
+		}
+		close(ch)
+	}()
+	return ch
+}
+
+// testSequentialReal performs network requests using CacheServer.GetRange.
+func testSequentialReal(t *testing.T, cs *CacheServer) {
 	for kr := range incomingRangeRequests() {
 		start := time.Now()
-		body, err := cs.GetRange(kr)
+		body, err := cs.GetRange(url, kr)
 		if err != nil {
 			log.Print(err)
 			continue
@@ -44,7 +67,96 @@ func testSequential(t *testing.T, cs *CacheServer) {
 	}
 }
 
-func TestSequential(t *testing.T) {
-	cs := NewCacheServer(url, nbytesMax)
-	testSequential(t, cs)
+// testConcurrentReal performs network requests using CacheServer.GetRange.
+func testConcurrentReal(t *testing.T, cs *CacheServer) {
+	var n sync.WaitGroup
+	for kr := range incomingRangeRequests() {
+		n.Add(1) // add to wait group
+		go func(keyrange rangecache.Keyrange) {
+			defer n.Done() // defer done
+			start := time.Now()
+			body, err := cs.GetRange(url, keyrange)
+			if err != nil {
+				return
+			}
+			log.Printf("time: %s: GetRange(%v), %d bytes", time.Since(start), keyrange, len(body))
+		}(kr)
+	}
+	n.Wait() // wait for all requests to be done
+}
+
+// testSequentialFake does not rely on network.
+func testSequentialFake(t *testing.T, cs *CacheServer) {
+	for kr := range incomingRangeRequestsFake() {
+		start := time.Now()
+		body := getRangeValue(cs, kr)
+		log.Printf("time: %s: GetRange(%v), %d bytes", time.Since(start), kr, len(body))
+	}
+}
+
+// testConcurrentFake does not rely on network.
+func testConcurrentFake(t *testing.T, cs *CacheServer) {
+	var n sync.WaitGroup
+	for kr := range incomingRangeRequestsFake() {
+		n.Add(1) // add to wait group
+		go func(keyrange rangecache.Keyrange) {
+			defer n.Done() // defer done
+			start := time.Now()
+			body := getRangeValue(cs, keyrange)
+			log.Printf("time: %s: GetRange(%v), %d bytes", time.Since(start), keyrange, len(body))
+		}(kr)
+	}
+	n.Wait() // wait for all requests to be done
+}
+
+// getRangeValues mirrors CacheServer.GetRange, but stubs the network call
+// by calling the generateValue helper.
+func getRangeValue(cs *CacheServer, keyrange rangecache.Keyrange) []int {
+	timeStart := time.Now()
+	v, ok := cs.get(keyrange)
+	if ok { // cache hit
+		body := v.([]int)
+		log.Printf("cache hit: %s, GET: %v\n", time.Since(timeStart), keyrange)
+		return body
+	}
+
+	// cache miss; make request
+	body := generateValue(keyrange)
+	cs.add(keyrange, body)
+	log.Printf("cache miss: %s, GET: %v\n", time.Since(timeStart), keyrange)
+	return body
+}
+
+// generateValue is a helper function that associats the value of a
+// key range to a sequence of integers from start to end, inclusive.
+func generateValue(kr rangecache.Keyrange) []int {
+	total := kr.End - kr.Start
+	start := kr.Start
+
+	krValue := make([]int, total+1)
+	for i := 0; i <= total; i++ {
+		krValue[i] = start + i
+		time.Sleep(1 * time.Nanosecond)
+	}
+	return krValue
+}
+
+func TestSequentialReal(t *testing.T) {
+	cs := NewCacheServer(nbytesMax)
+	testSequentialReal(t, cs)
+}
+
+func TestConcurrentReal(t *testing.T) {
+	cs := NewCacheServer(nbytesMax)
+	testConcurrentReal(t, cs)
+}
+
+func TestSequentialFake(t *testing.T) {
+	cs := NewCacheServer(nbytesMax)
+	testSequentialFake(t, cs)
+}
+
+func TestConcurrentFake(t *testing.T) {
+	cs := NewCacheServer(nbytesMax)
+	testConcurrentFake(t, cs)
 }
